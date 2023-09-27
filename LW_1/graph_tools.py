86745ab7a1d65f79e62f82bd669b2f5dd6d9c0ab
Vladimir Hmelnitski <vladimir.hmelnitski@booksrun.com>
2023-09-28 01:22:43 +0300

    Add functions to define graph classes

--- LW_1/graph_tools.py
+++ LW_1/graph_tools.py
@@ -6,16 +6,22 @@ class Graph:
         self.adjacency_matrix = None
         self.adjacency_list = None
         self.type = None
+        self.in_vertex_degrees = None
+        self.out_vertex_degrees = None
+        self.order = None
 
     def read_adjacency_matrix_from_file(self, file_obj):
-        self.adjacency_matrix = []
+        adj_matrix = []
         for line in file_obj.readlines():
             row = [int(el) for el in line.strip().split(" ")]
-            self.adjacency_matrix.append(row)
+            adj_matrix.append(row)
 
-        self.adjacency_matrix = np.array(self.adjacency_matrix)
+        self.adjacency_matrix = np.array(adj_matrix)
         self.adjacency_list = self.get_adjacency_list(self.adjacency_matrix)
         self.type = self.define_graph_type(self.adjacency_matrix)
+        self.order = self.adjacency_matrix.shape[0]
+        self.in_vertex_degrees = self.define_graph_vertex_degrees(self.adjacency_matrix, mode="in")
+        self.out_vertex_degrees = self.define_graph_vertex_degrees(self.adjacency_matrix, mode="out")
 
     @staticmethod
     def get_adjacency_list(adjacency_matrix) -> dict:
@@ -30,54 +36,43 @@ class Graph:
     def define_graph_type(adjacency_matrix) -> str:
         return "non-digraph" if (adjacency_matrix == adjacency_matrix.T).all() else "digraph"
 
-
-# def graph_is_connected(adjacency_list, start_vertex=1, visited=None) -> bool:
-#     if visited is None:
-#         visited = set()
-#     visited.add(start_vertex)
-#     for vertex in adjacency_list[start_vertex] - visited:
-#         graph_is_connected(adjacency_list, vertex, visited)
-#     return len(visited) == len(adjacency_list.keys())
-#
-#
-# def define_graph_vertex_degrees(adjacency_matrix):
-#     return np.sum(adjacency_matrix, axis=1)
-#
-#
-# def graph_is_euler_cycle(adjacency_matrix) -> bool:
-#     adjacency_list = get_adjacency_list(adjacency_matrix)
-#     if graph_is_connected(adjacency_list):
-#         vertex_degrees = define_graph_vertex_degrees(adjacency_matrix)
-#         return sum(vertex_degrees % 2 == 1) == 0
-#     return False
-#
-#
-# def graph_is_full(adjacency_matrix) -> bool:
-#     return 0 in adjacency_matrix
-#
-#
-# def graph_is_reflective(adjacency_matrix) -> bool:
-#     return 0 not in np.diag(adjacency_matrix)
-#
-#
-# def graph_is_transitive(adjacency_matrix) -> bool:
-#     adjacency_list = get_adjacency_list(adjacency_matrix)
-#     for base_vertex in adjacency_list.keys():
-#         for secondary_vertex in adjacency_list[base_vertex]:
-#             if len(adjacency_list[secondary_vertex] - adjacency_list[base_vertex] - {base_vertex}) != 0:
-#                 return False
-#     return True
-#
-#
-# def graph_is_tree(adjacency_matrix) -> bool:
-#     adjacency_list = get_adjacency_list(adjacency_matrix)
-#     if graph_is_connected(adjacency_list):
+    @staticmethod
+    def define_graph_vertex_degrees(adjacency_matrix, mode="in"):
+        axis = None
+        if mode == "in": axis = 1
+        elif mode == "out": axis = 0
+        return np.sum(adjacency_matrix, axis=axis)
 
 
+def graph_is_full(graph_obj) -> bool:
+    return (graph_obj.in_vertex_degrees == graph_obj.order - 1).all()
 
-with open("graphs/graph_1.txt", "r") as file:
-    graph = Graph()
-    graph.read_adjacency_matrix_from_file(file)
-    print(graph.adjacency_matrix == graph.adjacency_matrix.T)
-    print(graph.adjacency_list)
-    print(graph.type)
+
+def graph_is_reflective(graph_obj) -> bool:
+    return 0 not in np.diag(graph_obj.adjacency_matrix)
+
+
+def graph_is_connected(graph_obj, start_vertex=1, visited=None) -> bool:
+    if visited is None:
+        visited = set()
+    visited.add(start_vertex)
+    for vertex in graph_obj.adjacency_list[start_vertex] - visited:
+        graph_is_connected(graph_obj, vertex, visited)
+    return len(visited) == len(graph_obj.adjacency_list.keys())
+
+
+def graph_is_euler_cycle(graph_obj) -> bool:
+    if graph_is_connected(graph_obj):
+        if graph_obj.type == "non-digraph":
+            return sum(graph_obj.in_vertex_degrees % 2 == 1) == 0
+        elif graph_obj.type == "digraph":
+            return (graph_obj.in_vertex_degrees == graph_obj.out_vertex_degrees).all()
+    return False
+
+
+def graph_is_transitive(graph_obj) -> bool:
+    for base_vertex in graph_obj.adjacency_list.keys():
+        for secondary_vertex in graph_obj.adjacency_list[base_vertex]:
+            if len(graph_obj.adjacency_list[secondary_vertex] - graph_obj.adjacency_list[base_vertex] - {base_vertex}) != 0:
+                return False
+    return True
